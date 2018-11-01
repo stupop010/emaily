c8bfa99e5b6d1ac223901c55d8cb72a5eb27eeaf
Stuart Doney <stu@stuarts-air.home>
2018-11-01 21:04:51 +0000

    finnished off flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+const prodKeys = require('./prod')
+
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod')
+} else {
+    module.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
+require('./models/user');
+require('./services/passport');
+const keys = require('./config/keys');
+
+
+mongoose.connect(keys.mongoDB);
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-})
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('app running on port 3000')
-})
+})  
