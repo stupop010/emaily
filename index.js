8dcf317c8ae1363087ca82325e369262f85a004d
Stuart <stupop010@hotmail.co.uk>
2018-11-16 16:30:48 +0000

    added billing server front and back

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 
 require('./models/user');
 require('./services/passport');
 const keys = require('./config/keys');
 
 
-mongoose.connect(keys.mongoDB);
+mongoose.connect(keys.mongoDB, { useNewUrlParser: true });
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,8 +24,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__direname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('app running on port 3000')
+    console.log('app running on port 5000')
 })  
