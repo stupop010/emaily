11aacc2d70c4f25cb0298d44b352bbd1af283e71
Stuart <stupop010@hotmail.co.uk>
2018-12-05 16:18:14 +0000

    added mail

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,7 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-
-const Dashboard = () => <h2>Dashbroad</h2>
+import Dashboard from './dashboard';
 const SurveyNew = () => <h2>SurveyNew</h2>
 
 class App extends Component{
